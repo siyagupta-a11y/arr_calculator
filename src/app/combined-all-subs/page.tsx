83de82823ec966c7a31a85eaa4153e7691865e6b
Siya Gupta <siya.gupta@Siyas-Air.localdomain>
2026-03-18 14:27:06 -0400

    Render combined-all-subs as full page instead of redirect

--- src/app/combined-all-subs/page.tsx
+++ src/app/combined-all-subs/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function CombinedAllSubsAliasPage() {
-  redirect("/combined-billing-overview");
-}
+export { default } from "../combined-billing-overview/page";
